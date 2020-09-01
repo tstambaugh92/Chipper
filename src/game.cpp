7f6b905b14fd7dec1bd14891822e1ab8abd88372
Thomas Stambaugh <stambaugh.tom@gmail.com>
2020-08-31 21:07:19 -0500

    Game window closes when hitting x

--- src/game.cpp
+++ src/game.cpp
@@ -10,8 +10,15 @@ int main() {
     }
 
     SDL_Window * window = NULL;
-    window = SDL_CreateWindow( "Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 64*4, 32*4, SDL_WINDOW_SHOWN );
-    int x;
-    std::cin >> x;
+    window = SDL_CreateWindow( "CYNDI - Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 64*8, 32*8, SDL_WINDOW_SHOWN );
+    SDL_Event event;
+    bool quit = false;
+
+    while(!quit) {
+        while(SDL_PollEvent(&event) != 0) {
+            if(event.type == SDL_QUIT)
+                quit = true;
+        }
+    }
     return 0;
 }
